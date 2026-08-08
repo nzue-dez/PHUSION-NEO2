acd646656219d732831b2268f7512fe866419b07
Désiré Kouadio <kouadio.desire1@outlook.fr>
2026-08-08 03:10:17 +0200

    feat: variable dépendante Surv_without_dbp36sa + README et analyse.R mis à jour

--- analyse.R
+++ analyse.R
@@ -5,5 +5,8 @@
 
 source(here::here("R/load_functions.R"))
 source(here::here("R/data_management.R"))
+source(here::here("R/descriptive.R"))
+source(here::here("R/missing_analysis.R"))
 source(here::here("R/statistical_models.R"))
+source(here::here("R/model_diagnostics.R"))
 source(here::here("R/visualization.R"))
